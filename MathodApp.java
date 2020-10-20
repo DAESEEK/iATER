a2725046f6d773a01247de6f7352fc99e91ebfb5
DAESEEK <69086490+DAESEEK@users.noreply.github.com>
2020-10-20 20:35:16 +0700

    Add files via upload

---
+++ MathodApp.java
@@ -0,0 +1,20 @@
+package studyLessonBasic;
+
+public class MathodApp {
+
+	public static void printTimesA() {
+		System.out.printf("0");
+		System.out.printf("*");
+		System.out.printf("-\n");
+	}
+	
+	public static void main(String[] args) {
+		
+		for (int i=0; i<10; i++) {
+		 printTimesA();
+		}
+		
+		// 100 times
+	}
+
+}
