c96f415bb47e53ab99d3447c33e9207301a68c41
DAESEEK <69086490+DAESEEK@users.noreply.github.com>
2020-12-22 16:02:43 +0700

    Add files via upload

---
+++ GUILogin.java
@@ -0,0 +1,82 @@
+package OOP;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class GUILogin  implements ActionListener {
+	 
+	  private static JLabel userLabel;
+	  private static JTextField userText;
+	  private static JLabel passwordLabel;
+	  private static JTextField passwordText;
+	  private static JButton button;
+	  private static JLabel success;
+	
+	// https://beginnersbook.com/2015/07/java-swing-tutorial/
+
+	public static void main(String[] args) {
+		
+		JPanel panel = new JPanel();
+		JFrame frame = new JFrame();
+		frame.setSize(350, 200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// frame.setVisible(true);
+		frame.add(panel);
+		
+		panel.setLayout(null);
+		
+		userLabel = new JLabel("User");
+		userLabel.setBounds(10,20,80,25);
+		panel.add(userLabel);
+		
+		userText = new JTextField(20);
+		userText.setBounds(100,20,165,25);
+		panel.add(userText);
+		
+		passwordLabel = new JLabel("Password");
+		passwordLabel.setBounds(10,50,80,25);
+		panel.add(passwordLabel);
+		
+		passwordText = new JPasswordField();
+		passwordText.setBounds(100,50,165,25);
+		panel.add(passwordText);
+		
+		button = new JButton("Login");
+		button.setBounds(10,80,80,25);
+		button.addActionListener(new GUILogin());
+		panel.add(button);
+		
+		success = new JLabel("");
+		success.setBounds(10,110,300,25);
+		panel.add(success);
+		//success.setText
+		
+		frame.setVisible(true);	
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		String user = userText.getText();
+		String password = passwordText.getText();
+		System.out.println(user+", "+ password);
+		
+		
+		if(user.contentEquals("David")&&password.contentEquals("qwer!@#$")) {
+			success.setText("Login successful!");
+		}
+		else {
+			success.setText("login fail");
+			
+		}
+	}
+
+}
