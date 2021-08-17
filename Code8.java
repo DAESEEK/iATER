73596139d4c3ede617bc320aa969ca3a289f7cc8
DAESEEK <69086490+DAESEEK@users.noreply.github.com>
2021-08-17 15:47:44 +0700

    Add files via upload

--- Code8.java
+++ Code8.java
@@ -1,3 +1,6 @@
+/*
+ * 사용자로 부터 n개의 정수를 입력받은후 합과 최대 값을 구하여 출력하는 코드이다.
+ */
 package begin;
 
 import java.util.Scanner;
@@ -5,25 +8,27 @@ import java.util.Scanner;
 public class Code8 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner kb = new Scanner(System.in);
-		int n =kb.nextInt();
-		int []data = new int[n];
 		
-		for (int i= 0;i <n ; i++ ) {
-			data[i]= kb.nextInt();
+		int n = kb.nextInt();
+	
+		int[] data = new int[n];
+
+			for (int i = 0; i < n; i++) {
+			data[i] = kb.nextInt();
 			kb.close();
+
+			int sum = 0;
+			int max = 0;
 			
-		int sum =0, max = data[0];
-		for (int i1 =0; i1<n; i1++) {
-			sum +=data[i1];
-			if(data[i1]>max) max=data[i1];
-			
-		}
-		System.out.println("The sum is "+sum+" and the maximum is "+ max);
-		}
+			for (int i = 0; i < n; i++) {
+				sum += data[i];
 				
-		
+			}
+			System.out.println("The sum is " + sum + " and the maximum is " + max);
+		}
+
 	}
 
 }
